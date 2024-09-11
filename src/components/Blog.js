ce47d22b891afc1ab4cd3510801b7972e37d345a
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2024-09-11 10:40:35 -0400

    mobile styling

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
-const markdown = `# Hi, *Venus!*`
+const markdown = `# Hi, *Venus!*`;
 
 const Blog = () => {
-	return (
-		<Markdown>{markdown}</Markdown>
-	);
-}
+  return <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>;
+};
 
 export default Blog;
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,38 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import styles from "../styles/ProjectCard.module.css"
+import styles from "../styles/ProjectCard.module.css";
 import VariableContext from "../context/VariableProvider";
 
-const ProjectCard = ({children, title, url, imageUrl, altTxt}) => {
-	const { isMobile } = useContext(VariableContext);
+const ProjectCard = ({ children, title, url, imageUrl, altTxt }) => {
+  const { isMobile } = useContext(VariableContext);
 
-	if (isMobile) return (
-		<div className={styles.cardMobile}>
-			<h2 className={styles.blue}>{title}</h2>
-			<div className={styles.containerMobile}>
-				<Link to={url} target="_blank">
-					<img src={imageUrl} width="400px" height="220px" alt={altTxt}/>
-				</Link>
-				<div className={styles.childrenMobile}>
-					{children}
-				</div>
-			</div>
-		</div>
-	);
+  if (isMobile)
+    return (
+      <div className={styles.cardMobile}>
+        <h2 className={styles.blue}>{title}</h2>
+        <div className={styles.containerMobile}>
+          <Link to={url} target="_blank">
+            <img src={imageUrl} width="100%" alt={altTxt} />
+          </Link>
+          <div className={styles.childrenMobile}>{children}</div>
+        </div>
+      </div>
+    );
 
-	return (
-		<div className={styles.card}>
-			<h2 className={styles.blue}>{title}</h2>
-			<div className={styles.container}>
-				<Link to={url} target="_blank">
-					<img src={imageUrl} width="400px" height="220px" alt={altTxt}/>
-				</Link>
-				<div className={styles.children}>
-					{children}
-				</div>
-			</div>
-		</div>
-	);
-}
+  return (
+    <div className={styles.card}>
+      <h2 className={styles.blue}>{title}</h2>
+      <div className={styles.container}>
+        <Link to={url} target="_blank">
+          <img src={imageUrl} width="400px" height="220px" alt={altTxt} />
+        </Link>
+        <div className={styles.children}>{children}</div>
+      </div>
+    </div>
+  );
+};
 
 export default ProjectCard;
